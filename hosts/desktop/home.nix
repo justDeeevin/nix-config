af43a29a2391d136f3c92631cdd43a5099d2164f
Devin Droddy <devin.droddy@gmail.com>
2024-02-26 19:46:00 -0500

    Add nix-index

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -57,6 +57,7 @@
     pkg-config
     thefuck
     clang
+    nix-index
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
