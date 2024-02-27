06cd8f5f54d152e94bacc47cc415cdb86c0e76e4
Devin Droddy <devin.droddy@gmail.com>
2024-02-26 20:57:57 -0500

    add libraries to system packages to maybe get pkg-config good

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -101,7 +101,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    openssl
+    xorg.libX11.dev
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
