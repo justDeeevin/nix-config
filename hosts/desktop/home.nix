c8de1a0cc021a49de9449f47fde3ce1907133d90
Devin Droddy <devin.droddy@gmail.com>
2024-02-28 21:08:32 -0500

    add ntfs3g

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -54,6 +54,7 @@
     thefuck
     clang
     nix-index
+    ntfs3g
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
