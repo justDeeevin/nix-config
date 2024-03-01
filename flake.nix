3f4a82d2b2e586bb7699b93d09b7f8159cbf47c6
Devin Droddy <devin.droddy@gmail.com>
2024-02-29 21:04:12 -0500

    fix version typo

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/release-21.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/release-23.11";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
