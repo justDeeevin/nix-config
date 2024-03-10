0d415e8c127a0c955ca04c15eb932c4fd032dca8
Devin Droddy <devin.droddy@gmail.com>
2024-03-09 20:22:00 -0500

    add ripgrep

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -55,6 +55,7 @@
     wl-clipboard
     mangohud
     just
+    ripgrep
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
