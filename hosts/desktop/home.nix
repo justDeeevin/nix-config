ecaf0f76ada4a19dc4e5c852e71a6a546b97d844
Devin Droddy <devin.droddy@gmail.com>
2024-03-19 18:41:49 -0400

    add playerctl and dunst

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -52,6 +52,8 @@
     libnotify
     swww
     rofi-wayland
+    playerctl
+    dunst
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
