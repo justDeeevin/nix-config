f68c9a1b57d7a8c4252041bd7b6e303b4a8661fc
Devin Droddy <devin.droddy@gmail.com>
2024-03-30 14:39:35 -0400

    switch to swaylock-effects

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -55,7 +55,7 @@
     swaynotificationcenter
     swww
     cinnamon.nemo
-    swaylock
+    swaylock-effects
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
