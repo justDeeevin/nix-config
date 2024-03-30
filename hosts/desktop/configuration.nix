4e6b5c2b11f02d2a195b6cfe67c306b8b9f1fbe6
Devin Droddy <devin.droddy@gmail.com>
2024-03-30 12:33:21 -0400

    remove plasma6

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -54,7 +54,6 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
