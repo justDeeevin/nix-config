28318129d1f15556e172df8ea2d0caaa28a45836
Devin Droddy <devin.droddy@gmail.com>
2024-05-14 15:53:16 -0400

    add chromium
    I need it for homework

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -50,6 +50,7 @@
     neovim
     obsidian
     wineWowPackages.waylandFull
+    chromium
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
