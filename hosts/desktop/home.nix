5fedc7d4e092f76cace107459617361b988a41dd
Devin Droddy <devin.droddy@gmail.com>
2024-06-28 17:34:06 -0400

    add gzdoom

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -54,6 +54,7 @@
     inputs.drg-mod-manager.packages.x86_64-linux.default
     kdePackages.kdenlive
     BeatSaberModManager
+    gzdoom
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
