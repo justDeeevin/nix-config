2a9a706905787b72f15aa8b2a575a2fa0be11118
Devin Droddy <devin.droddy@gmail.com>
2024-08-02 13:37:11 -0400

    add slack

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -57,6 +57,7 @@
     gzdoom
     libreoffice
     clonehero
+    slack
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
