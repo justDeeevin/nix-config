95472298849786d231e74ecd6362052967cd867d
Devin Droddy <devin.droddy@gmail.com>
2024-08-02 13:39:33 -0400

    add zed

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -58,6 +58,7 @@
     libreoffice
     clonehero
     slack
+    zed-editor
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
