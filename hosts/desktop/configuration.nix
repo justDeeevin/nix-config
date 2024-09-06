10cb5df4ac4ba1d30cee2787b60e72280b810dca
Devin Droddy <devin.droddy@gmail.com>
2024-09-05 20:34:11 -0400

    add archon bootstrap config

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -13,4 +13,17 @@
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true;
   programs.gamemode.enable = true;
+
+  environment.etc."archon/bootstrap.json" = {
+    enable = true;
+    text  = builtins.toJSON {
+      nodes = [
+        {
+          name = "devin-pc";
+          ip = "127.0.0.1";
+          port = 8888;
+        }
+      ];
+    };
+  };
 }
