7466da756914077663b3a907ead50c2a326d001d
Devin Droddy <devin.droddy@gmail.com>
2024-10-01 10:12:56 -0400

    add freecad

--- global/home.nix
+++ global/home.nix
@@ -54,6 +54,7 @@
     obsidian
     chromium
     mindustry
+    freecad
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
