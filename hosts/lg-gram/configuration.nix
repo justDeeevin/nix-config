ca1afef9fb697706d621c5cd3c1d123e007be085
Devin Droddy <devin.droddy@gmail.com>
2024-10-07 22:30:59 -0400

    rename `windscribeVPN.conf` to `windscribeVPN.ovpn`

--- hosts/lg-gram/configuration.nix
+++ hosts/lg-gram/configuration.nix
@@ -2,5 +2,5 @@
   imports = [./hardware-configuration.nix ./nvidia-prime.nix];
   networking.hostName = "devin-gram"; # Define your hostname.
 
-  services.openvpn.servers.windscribe.config = "config ${./windscribeVPN.conf}";
+  services.openvpn.servers.windscribe.config = "config ${./windscribeVPN.ovpn}";
 }
