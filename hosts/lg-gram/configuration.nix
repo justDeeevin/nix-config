44280cee52bec0275542326c22fbb9e68a71293e
Devin Droddy <devin.droddy@gmail.com>
2024-10-07 22:24:05 -0400

    add windscribe vpn (provided by Ellie <3) to lg-gram
    now i can (probably) work on pyro stuff at school

--- hosts/lg-gram/configuration.nix
+++ hosts/lg-gram/configuration.nix
@@ -1,4 +1,6 @@
 {
   imports = [./hardware-configuration.nix ./nvidia-prime.nix];
   networking.hostName = "devin-gram"; # Define your hostname.
+
+  services.openvpn.servers.windscribe.config = "config ${./windscribeVPN.conf}";
 }
