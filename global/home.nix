fcb7d30a9c7150a672b4a164ef33894586be830b
Devin Droddy <devin.droddy@gmail.com>
2024-10-26 21:00:06 -0400

    add kdenlive

--- global/home.nix
+++ global/home.nix
@@ -43,6 +43,7 @@
     obsidian
     chromium
     quickemu
+    kdenlive
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
