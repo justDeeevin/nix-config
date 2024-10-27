23237673e2fa6028670bc102c94844c78483fb39
Devin Droddy <devin.droddy@gmail.com>
2024-10-26 20:59:03 -0400

    add quickemu

--- global/home.nix
+++ global/home.nix
@@ -42,6 +42,7 @@
     inputs.zen-browser.packages.x86_64-linux.default
     obsidian
     chromium
+    quickemu
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
