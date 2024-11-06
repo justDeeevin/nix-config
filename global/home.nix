a992d5cbc7becda1b99356d6c85c1a7a870164d3
Devin Droddy <devin.droddy@gmail.com>
2024-11-06 00:13:11 -0500

    add mattermost

--- global/home.nix
+++ global/home.nix
@@ -44,6 +44,7 @@
     chromium
     quickemu
     kdenlive
+    mattermost-desktop
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
