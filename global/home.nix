f1fffd49506020c56700d9af3ee4e07425b003ae
Devin Droddy <devin.droddy@gmail.com>
2024-11-12 19:24:54 -0500

    finished posting config

--- global/home.nix
+++ global/home.nix
@@ -273,6 +273,28 @@
     enable = true;
     settings = {
       animation = "full";
+      theme = "oxocarbon";
     };
+    themes = [
+      {
+        name = "oxocarbon";
+        # base08
+        primary = "#3ddbd9";
+        # base0F
+        secondary = "#82cfff";
+        # base0C
+        accent = "#ff7eb6";
+        # base00
+        background = "#161616";
+        # base02
+        surface = "#393939";
+        # base0A
+        error = "#ee5396";
+        # base0D
+        success = "#42be65";
+        # base0E
+        warning = "#be95ff";
+      }
+    ];
   };
 }
