df1c8c0c3523296bfe89ca737502a913205ac85d
Devin Droddy <devin.droddy@gmail.com>
2024-11-15 10:58:44 -0500

    url-fetched nixos icon

--- global/home.nix
+++ global/home.nix
@@ -92,9 +92,11 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-    "Pictures" = {
-      recursive = true;
-      source = ./Pictures;
+    "Pictures/nixos-logo.png" = {
+      source = pkgs.fetchurl {
+        url = "https://static-00.iconduck.com/assets.00/nixos-icon-2048x1776-8czr8nir.png";
+        hash = "sha256-GX9Zv+Pf3BdrdvtqJ1PlnW+gkAMuC9H8YCq05sDmWMI=";
+      };
     };
   };
 
