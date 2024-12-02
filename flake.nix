b45345e6bb82ea1e9ea66ffdc3317a03761f66b6
Devin Droddy <devin.droddy@gmail.com>
2024-12-02 12:33:53 -0500

    update zen flake

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     };
 
     zen-browser = {
-      url = "github:marcecoll/zen-browser-flake";
+      url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
