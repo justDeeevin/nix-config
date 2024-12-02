e1363cb35a4a96502e201bfc7986a079be2b5a50
Devin Droddy <devin.droddy@gmail.com>
2024-12-02 11:34:54 -0500

    hyprland touchpad config for laptop

--- hosts/lg-gram/home.nix
+++ hosts/lg-gram/home.nix
@@ -4,7 +4,14 @@
   ...
 }: {
   imports = [./plasma.nix];
-  wayland.windowManager.hyprland.settings.exec-once = [
-    "${lib.getExe pkgs.networkmanagerapplet}"
-  ];
+  wayland.windowManager.hyprland.settings = {
+    exec-once = [
+      "${lib.getExe pkgs.networkmanagerapplet}"
+    ];
+
+    input.touchpad = {
+      natural_scroll = true;
+      scroll_factor = 0.65;
+    };
+  };
 }
