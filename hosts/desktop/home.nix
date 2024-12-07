3520982f77df6260d3b45021a71783a269410d14
Devin Droddy <devin.droddy@gmail.com>
2024-12-06 22:17:10 -0500

    remove unneeded pkgs on desktop

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -1,10 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     ntfs3g
-    mangohud
-    sidequest
-    BeatSaberModManager
-    slack
   ];
 
   wayland.windowManager.hyprland.settings.monitor = "HDMI-A-1, highres@highrr, 0x0, 1, transform, 3";
