1e08885e7145b7ecf155059f7479c151a5e7c498
Devin Droddy <devin.droddy@gmail.com>
2024-12-09 08:12:30 -0500

    remove unnecessary override on hypr-dymanic-cursors
    this update was recently merged into nixpkgs
    I'm officially a contributor!

--- global/rice/hyprland.nix
+++ global/rice/hyprland.nix
@@ -9,14 +9,7 @@ in {
     enable = true;
 
     plugins = with pkgs.hyprlandPlugins; [
-      (lib.overrideDerivation hypr-dynamic-cursors (oldAttrs: {
-        src = pkgs.fetchFromGitHub {
-          owner = "VirtCode";
-          repo = "hypr-dynamic-cursors";
-          rev = "81f4b964f997a3174596ef22c7a1dee8a5f616c7";
-          hash = "sha256-3SDwq2i2QW9nu7HBCPuDtLmrwLt2kajzImBsawKRZ+s=";
-        };
-      }))
+      hypr-dynamic-cursors
       hyprspace
     ];
 
