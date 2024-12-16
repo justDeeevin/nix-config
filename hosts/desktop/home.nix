dddb465b55915cb30dbf1edf7115a9e69852851c
Devin Droddy <devin.droddy@gmail.com>
2024-12-16 18:28:38 -0500

    slightly better monitor positioning on desktop

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -3,7 +3,10 @@
     ntfs3g
   ];
 
-  wayland.windowManager.hyprland.settings.monitor = "HDMI-A-1, highres@highrr, 0x0, 1, transform, 3";
+  wayland.windowManager.hyprland.settings.monitor = [
+    "HDMI-A-1, highres@highrr, 0x0, 1, transform, 3"
+    "DP-1, highres@highrr, 1080x240, 1"
+  ];
 
   programs.syspower.settings.monitor = 1;
 }
