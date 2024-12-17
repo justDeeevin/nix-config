38c4253b6220182bbed43bc59f2ee16df8118609
Devin Droddy <devin.droddy@gmail.com>
2024-12-17 07:49:12 -0500

    remove paning keybinds from wezterm config

--- global/wezterm.lua
+++ global/wezterm.lua
@@ -7,44 +7,6 @@ config.font = wezterm.font({
 	harfbuzz_features = { "calt", "ss01", "ss02", "ss03", "ss04", "ss05", "ss07", "ss08", "ss09", "liga" },
 })
 
-config.keys = {
-	{
-		key = "Enter",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.SplitHorizontal({}),
-	},
-	{
-		key = "Enter",
-		mods = "CTRL|ALT",
-		action = wezterm.action.SplitVertical({}),
-	},
-	{
-		key = "i",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "e",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "n",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Down"),
-	},
-	{
-		key = "m",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "t",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.SpawnCommandInNewTab({ cwd = wezterm.home_dir }),
-	},
-}
-
 local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 bar.apply_to_config(config)
 
