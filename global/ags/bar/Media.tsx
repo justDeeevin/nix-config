4c78fcef986593e86e751681612019f3d47c2a82
Devin Droddy <devin.droddy@gmail.com>
2025-01-13 10:36:15 -0500

    FINALLY working "Nothing Playing" detection for media bar module

--- global/ags/bar/Media.tsx
+++ global/ags/bar/Media.tsx
@@ -4,58 +4,73 @@ import Mpris from "gi://AstalMpris";
 
 export default ({ index }: { index: Variable<number> }) => {
   const mpris = Mpris.get_default();
-  const player = Variable.derive(
-    [bind(mpris, "players"), index()],
-    (ps, i) => ps[i],
+
+  const inner = Variable.derive(
+    [bind(mpris, "players"), index],
+    (players, i) => {
+      const p = players[i];
+
+      const label = Variable.derive(
+        [bind(p, "title"), bind(p, "artist")],
+        (title, artist) => `${title}${artist ? ` - ${artist}` : ""}`,
+      );
+
+      // When there is no active player, Mpris doesn't actually set the player to `null`. It just sets a handful of properties to `null`. Ultimately, if every single one of the below properties is null, even if there's an active player, we don't have anything to display and thus want to just say "nothing playing".
+      const properties_to_check: Array<keyof Mpris.Player> = [
+        "cover_art",
+        "title",
+        "artist",
+      ];
+
+      const exists = Variable.derive(
+        properties_to_check.map((key) => bind(p, key)),
+        (...props) => props.find((v) => v !== null && v !== "") !== undefined,
+      );
+
+      return (
+        <box>
+          {exists().as((e) =>
+            e ? (
+              <box>
+                <button onClicked={() => p.play_pause()} cursor="pointer">
+                  <label
+                    label={bind(p, "playback_status").as((s) => {
+                      const { PLAYING, PAUSED, STOPPED } = Mpris.PlaybackStatus;
+                      switch (s) {
+                        case PLAYING:
+                          return "󰏤";
+                        case PAUSED:
+                          return "󰐊";
+                        case STOPPED:
+                          return "󰓛";
+                      }
+                    })}
+                  />
+                </button>
+                <button
+                  onClicked={() => App.toggle_window("media")}
+                  cursor="pointer"
+                >
+                  <box>
+                    <box
+                      className="cover"
+                      valign={Gtk.Align.CENTER}
+                      css={bind(p, "coverArt").as(
+                        (cover) => `background-image: url('${cover}');`,
+                      )}
+                    />
+                    <label label={label()} truncate />
+                  </box>
+                </button>
+              </box>
+            ) : (
+              "Nothing Playing"
+            ),
+          )}
+        </box>
+      );
+    },
   );
-  const inner = player().as((p) => {
-    const label = Variable.derive(
-      [bind(p, "title"), bind(p, "artist")],
-      (title, artist) => {
-        return title
-          ? `${title}${artist ? ` - ${artist}` : ""}`
-          : "Nothing Playing";
-      },
-    );
 
-    return p ? (
-      <box>
-        {p && p.title && (
-          <button onClicked={() => p.play_pause()} cursor="pointer">
-            <label
-              label={bind(p, "playback_status").as((s) => {
-                const { PLAYING, PAUSED, STOPPED } = Mpris.PlaybackStatus;
-                switch (s) {
-                  case PLAYING:
-                    return "󰐊";
-                  case PAUSED:
-                    return "󰏤";
-                  case STOPPED:
-                    return "󰓛";
-                }
-              })}
-            />
-          </button>
-        )}
-        <button onClicked={() => App.toggle_window("media")} cursor="pointer">
-          <box>
-            {p && p.title && (
-              <box
-                className="cover"
-                valign={Gtk.Align.CENTER}
-                css={bind(p, "coverArt").as(
-                  (cover) => `background-image: url('${cover}');`,
-                )}
-              />
-            )}
-            <label label={label()} truncate />
-          </box>
-        </button>
-      </box>
-    ) : (
-      "Nothing Playing"
-    );
-  });
-
-  return <box className="media">{inner}</box>;
+  return <box className="media">{inner()}</box>;
 };
