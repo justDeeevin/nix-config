5ed7e80a317fb4627bd580cc38be4d8fe5a7dbea
Devin Droddy <devin.droddy@gmail.com>
2025-01-14 07:46:56 -0500

    autolaunch nm-applet on laptop

--- hosts/lg-gram/hypr.nix
+++ hosts/lg-gram/hypr.nix
@@ -4,6 +4,8 @@
   ...
 }: {
   wayland.windowManager.hyprland.settings = {
+    exec-once = [(lib.getExe pkgs.networkmanagerapplet)];
+
     bind = [
       ", xf86monbrightnessup, exec, ${lib.getExe pkgs.brightnessctl} set 10%+"
       ", xf86monbrightnessdown, exec, ${lib.getExe pkgs.brightnessctl} set 10%-"
