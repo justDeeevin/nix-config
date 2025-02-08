74351dad8d0ec40c0c5c9dc810c92660f3b82e5e
Devin Droddy <devin.droddy@gmail.com>
2025-02-07 19:31:42 -0500

    add lazygit.nvim

---
+++ global/nixvim/config/plugins/lazygit.nix
@@ -0,0 +1,12 @@
+{
+  programs.nixvim = {
+    plugins.lazygit.enable = true;
+    keymaps = [
+      {
+        key = "<leader>g";
+        action = "<cmd>LazyGit<CR>";
+        mode = "n";
+      }
+    ];
+  };
+}
