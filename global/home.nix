ef09c756e2798cf6fc7583b39c925a7a6ae15a6b
Devin Droddy <devin.droddy@gmail.com>
2025-02-10 08:28:46 -0500

    reinstall hyprland-qtutils

--- global/home.nix
+++ global/home.nix
@@ -56,6 +56,7 @@
     hyprpolkitagent
     ghostty
     telegram-desktop
+    hyprland-qtutils
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
