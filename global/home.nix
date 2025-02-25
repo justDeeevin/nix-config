d0afed1e12751e79a75f4257bc3fcd80b4fb0227
Devin Droddy <devin.droddy@gmail.com>
2025-02-25 08:53:50 -0500

    add godot back

--- global/home.nix
+++ global/home.nix
@@ -58,6 +58,7 @@
     telegram-desktop
     hyprland-qtutils
     cargo-generate
+    godot_4
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
