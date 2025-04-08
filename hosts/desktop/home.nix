8ec9e8cb3c2a84fe0a0cb26775e19a85709f1c82
Devin Droddy <devin.droddy@gmail.com>
2025-04-08 15:16:36 -0400

    desktop workspace rules to put them on the right monitors yaaaaahhh

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -2,10 +2,24 @@
 {
   home.packages = with pkgs; [ ntfs3g ];
 
-  wayland.windowManager.hyprland.settings.monitor = [
-    "HDMI-A-1, highres@highrr, 0x0, 1, transform, 3"
-    "DP-1, highres@highrr, 1080x240, 1"
-  ];
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "HDMI-A-1, highres@highrr, 0x0, 1, transform, 3"
+      "DP-1, highres@highrr, 1080x240, 1"
+    ];
+    workspace = [
+      "1, monitor:HDMI-A-1"
+      "2, monitor:HDMI-A-1"
+      "3, monitor:HDMI-A-1"
+      "4, monitor:DP-1"
+      "5, monitor:DP-1"
+      "6, monitor:DP-1"
+      "7, monitor:DP-1"
+      "8, monitor:DP-1"
+      "9, monitor:DP-1"
+      "10, monitor:DP-1"
+    ];
+  };
 
   programs.syspower.settings.monitor = 1;
 
