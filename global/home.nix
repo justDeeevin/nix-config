20b927a7738f537bb40eebcb82104e960252cb56
Devin Droddy <devin.droddy@gmail.com>
2025-05-07 15:29:14 -0400

    add r2modman + flake update

--- global/home.nix
+++ global/home.nix
@@ -63,6 +63,7 @@
     sops
     sccache
     inputs.view.packages.x86_64-linux.default
+    r2modman
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
