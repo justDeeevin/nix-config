51b607682fb39ca3caa88fd7bb66d878cff06bef
Devin Droddy <devin.droddy@gmail.com>
2025-06-09 22:30:11 -0400

    obsidian plugin is entirely nixos-config specific

--- global/home.nix
+++ global/home.nix
@@ -8,16 +8,56 @@
   ...
 }:
 let
-  nixvim = (
-    inputs.self.packages.x86_64-linux.nixvim.extend {
-      plugins.obsidian.settings.workspaces = [
-        {
-          name = "Third Brain";
-          path = "~/Documents/Third Brain";
-        }
-      ];
-    }
-  );
+  nixvim = inputs.self.packages.x86_64-linux.nixvim.extend {
+    plugins.obsidian = {
+      enable = true;
+      settings = {
+        workspaces = [
+          {
+            name = "Third Brain";
+            path = "~/Documents/Third Brain";
+          }
+        ];
+        daily_notes = {
+          folder = "Daily";
+          template = "Templates/Daily Note Template.md";
+          date_format = "%b %-d, %Y";
+        };
+        templates.subdir = "Templates";
+        disable_frontmatter = true;
+        attachments = {
+          img_folder = "Assets";
+          img_name_func.__raw = ''
+            function()
+              local date = os.date("*t")
+              return string.format("Pasted image %d%d%d%d%d%d", date.year, date.month, date.day, date.hour, date.min, date.sec)
+            end
+          '';
+          img_text_func.__raw = ''
+            function(client, path)
+              path = client:vault_relative_path(path) or path
+              return string.format("![[%s]]", path.name)
+            end
+          '';
+        };
+      };
+    };
+
+    opts = {
+      conceallevel = 2;
+    };
+
+    keymaps = [
+      {
+        key = "<leader>ot";
+        action = "<cmd>ObsidianToday<CR>";
+      }
+      {
+        key = "<leader>op";
+        action = "<cmd>ObsidianPasteImg<CR>";
+      }
+    ];
+  };
 in
 {
   imports = [
