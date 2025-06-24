405805bc2fef2f2952561b09abb8be0a7dca9d0b
Devin Droddy <devin.droddy@gmail.com>
2025-06-23 22:44:09 -0400

    add nautilus

--- global/home.nix
+++ global/home.nix
@@ -117,6 +117,7 @@ in
     nixvim
     rip2
     equibop
+    nautilus
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
