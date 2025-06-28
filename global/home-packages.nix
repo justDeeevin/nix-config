7a137f23d3ca2c70599a8d6be618a3a930050359
Devin Droddy <devin.droddy@gmail.com>
2025-06-28 12:42:32 -0400

    separate file for home.packages

---
+++ global/home-packages.nix
@@ -0,0 +1,48 @@
+{
+  pkgs,
+  lib,
+  inputs,
+  nixvim,
+  ...
+}:
+with pkgs;
+[
+  gh
+  youtube-music
+  wl-clipboard
+  just
+  vlc
+  ffmpeg-full
+  kdePackages.kalk
+  jdk
+  gradle
+  prismlauncher
+  wineWowPackages.waylandFull
+  unzip
+  tree-sitter
+  magic-wormhole
+  libreoffice
+  inputs.zen-browser.packages.x86_64-linux.default
+  obsidian
+  chromium
+  quickemu
+  kdePackages.kdenlive
+  nh
+  hyprpolkitagent
+  telegram-desktop
+  hyprland-qtutils
+  cargo-generate
+  # godot
+  typst
+  sops
+  timg
+  r2modman
+  (lib.hiPrio uutils-coreutils-noprefix)
+  tldr
+  gimp
+  bluetui
+  nixvim
+  rip2
+  equibop
+  nautilus
+]
