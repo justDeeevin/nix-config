811bfa5878ae864023478119f19672a83df32d49
Devin Droddy <devin.droddy@gmail.com>
2025-07-12 19:25:57 -0400

    ensure canon printer at home

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -14,4 +14,15 @@
 
   services.udev.packages = [ pkgs.qmk-udev-rules ];
 
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "Canon";
+        location = "Helen's Office";
+        deviceUri = "dnssd://Canon%20MF642C%2F643C%2F644C%20(e3%3Aac%3A28)%20(e3%3Aac%3A28)%20(5%20%20(e3%3Aac%3A28)%20(2)._ipp._tcp.local/?uuid=6d4ff0ce-6b11-11d8-8020-f4a997d1e389";
+        model = "everywhere";
+      }
+    ];
+    ensureDefaultPrinter = "Canon";
+  };
 }
