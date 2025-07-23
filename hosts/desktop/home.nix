98e08d92969824d4c4d1a97fd5449d1dcf40e317
Devin Droddy <devin@justdeeevin.dev>
2025-07-23 17:26:04 -0400

    Auto-start slack minimized on desktop

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -11,7 +11,10 @@
       (builtins.map (i: "${builtins.toString i}, monitor:HDMI-A-1") (lib.range 1 3))
       ++ (builtins.map (i: "${builtins.toString i}, monitor:DP-1") (lib.range 4 10));
     input.sensitivity = -0.4;
-    exec-once = [ "${lib.getExe pkgs.openrgb} -p ${./icy.orp}" ];
+    exec-once = [
+      "${lib.getExe pkgs.openrgb} -p ${./icy.orp}"
+      "slack --startup"
+    ];
   };
 
   programs.vscode.profiles.default = {
