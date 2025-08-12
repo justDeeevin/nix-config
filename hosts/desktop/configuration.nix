e8aab58d5462915c6f71c5a569bb232ca3a889ce
Devin Droddy <devin@justdeeevin.dev>
2025-08-12 12:50:23 -0400

    get rid of ensurePrinters

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -33,18 +33,6 @@ in
     probe-rs-udev-rules
   ];
 
-  hardware.printers = {
-    ensurePrinters = [
-      {
-        name = "Canon";
-        location = "Helen's Office";
-        deviceUri = "dnssd://Canon%20MF642C%2F643C%2F644C%20(e3%3Aac%3A28)%20(e3%3Aac%3A28)%20(5%20%20(e3%3Aac%3A28)%20(2)._ipp._tcp.local/?uuid=6d4ff0ce-6b11-11d8-8020-f4a997d1e389";
-        model = "everywhere";
-      }
-    ];
-    ensureDefaultPrinter = "Canon";
-  };
-
   hardware.openrazer = {
     enable = true;
     users = [ "devin" ];
