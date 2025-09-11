c28884639d0f6b10f0f14ace91ba454b7291685f
Devin Droddy <devin@justdeeevin.dev>
2025-09-11 10:38:54 -0400

    add battery percentage to laptop lockscreen

--- hosts/lg-gram/hypr.nix
+++ hosts/lg-gram/hypr.nix
@@ -20,4 +20,15 @@
     };
   };
 
+  programs.hyprlock.settings.label = [
+    {
+      text = ''
+        cmd[update:1000] echo "󰁹 $(cat /sys/class/power_supply/BAT0/capacity)%"
+      '';
+      position = "0, 6%";
+      font_size = 35;
+      font_family = "Monaspace neon";
+      halign = "center";
+    }
+  ];
 }
