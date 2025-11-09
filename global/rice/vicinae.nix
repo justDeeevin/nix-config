bd848b45764ff08a06632fb861da4cfeefc6dfde
Devin Droddy <devin@justdeeevin.dev>
2025-11-09 17:28:13 -0500

    update home-manager
    nix-community/home-manager#8141 fixes `programs.vicinae.themes` :3c

--- global/rice/vicinae.nix
+++ global/rice/vicinae.nix
@@ -3,47 +3,45 @@
   programs.vicinae = {
     enable = true;
     systemd.enable = true;
-  };
+    themes.oxocarbon = {
+      meta = {
+        version = 1;
+        name = "Oxocarbon";
+        description = "High contrast accessible colorscheme inspired by IBM Carbon";
+        variant = "dark";
+        icon = pkgs.fetchurl {
+          url = "https://avatars.githubusercontent.com/u/120229920";
+          hash = "sha256-T6c2JMvRvwIiSNoY1cO4co1kRdtjLrixsoBkZKX1YAE=";
+        };
+      };
 
-  xdg.dataFile."vicinae/themes/oxocarbon.toml".source =
-    (pkgs.formats.toml { }).generate "vicinae-oxocarbon-theme"
-      {
-        meta = {
-          version = 1;
-          name = "Oxocarbon";
-          description = "High contrast accessible colorscheme inspired by IBM Carbon";
-          variant = "dark";
-          icon = pkgs.fetchurl {
-            url = "https://avatars.githubusercontent.com/u/120229920";
-            hash = "sha256-T6c2JMvRvwIiSNoY1cO4co1kRdtjLrixsoBkZKX1YAE=";
+      colors =
+        let
+          colors = (config.stylix.base16.mkSchemeAttrs config.stylix.base16Scheme).withHashtag;
+        in
+        {
+          core = rec {
+            background = colors.base00;
+            foreground = colors.base06;
+            secondary_background = colors.base04;
+            border = colors.base03;
+            accent = colors.orange;
+            accent_foreground = foreground;
+          };
+
+          accents = {
+            inherit (colors)
+              blue
+              green
+              orange
+              red
+              yellow
+              cyan
+              magenta
+              ;
           };
         };
+    };
 
-        colors =
-          let
-            colors = (config.stylix.base16.mkSchemeAttrs config.stylix.base16Scheme).withHashtag;
-          in
-          {
-            core = rec {
-              background = colors.base00;
-              foreground = colors.base06;
-              secondary_background = colors.base04;
-              border = colors.base03;
-              accent = colors.orange;
-              accent_foreground = foreground;
-            };
-
-            accents = {
-              inherit (colors)
-                blue
-                green
-                orange
-                red
-                yellow
-                cyan
-                magenta
-                ;
-            };
-          };
-      };
+  };
 }
