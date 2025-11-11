a3121fd52374843f49c30e627e626f8652dbd900
Devin Droddy <devin@justdeeevin.dev>
2025-11-11 10:42:45 -0500

    update higgs wm config

--- hosts/higgs/home.nix
+++ hosts/higgs/home.nix
@@ -1,7 +1,28 @@
 {
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
   imports = [
     ./hypr.nix
   ];
   sops.secrets.OPENAI_API_KEY.sopsFile = ./secrets.yaml;
-  programs.ashell.settings.settings.lock_cmd = "hyprlock &";
+
+  programs.niri.settings.binds =
+    let
+      inherit (config.lib.niri.actions) spawn;
+      brightnessctl = lib.getExe pkgs.brightnessctl;
+    in
+    {
+      "XF86MonBrightnessUp" = {
+        action = spawn brightnessctl "set" "+5%";
+        allow-when-locked = true;
+      };
+      "XF86MonBrightnessDown" = {
+        action = spawn brightnessctl "set" "5%-";
+        allow-when-locked = true;
+      };
+    };
 }
--- hosts/higgs/hypr.nix
+++ hosts/higgs/hypr.nix
@@ -1,33 +1,4 @@
-{ pkgs, lib, ... }:
 {
-  wayland.windowManager.hyprland.settings = {
-    bind = [
-      ", xf86monbrightnessup, exec, ${lib.getExe pkgs.brightnessctl} set 10%+"
-      ", xf86monbrightnessdown, exec, ${lib.getExe pkgs.brightnessctl} set 10%-"
-      "$mod, L, exec, loginctl lock-session"
-    ];
-
-    monitor = ",preferred,auto,1";
-
-    input.touchpad = {
-      natural_scroll = true;
-      scroll_factor = 0.65;
-      clickfinger_behavior = true;
-    };
-
-    gesture =
-      let
-        playerctl = lib.getExe pkgs.playerctl;
-      in
-      [
-        "3, horizontal, workspace"
-        "4, left, dispatcher, exec, ${playerctl} previous"
-        "4, down, dispatcher, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-        "4, up, dispatcher, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-        "4, right, dispatcher, exec, ${playerctl} next"
-      ];
-  };
-
   programs.hyprlock.settings.label = [
     {
       text = ''
