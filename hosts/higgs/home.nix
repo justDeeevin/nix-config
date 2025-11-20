bd8478867303bd502171085f3f1630e251fea840
Devin Droddy <devin@justdeeevin.dev>
2025-11-20 09:46:43 -0500

    higgs niri backgdrop color

--- hosts/higgs/home.nix
+++ hosts/higgs/home.nix
@@ -10,19 +10,22 @@
   ];
   sops.secrets.OPENAI_API_KEY.sopsFile = ./secrets.yaml;
 
-  programs.niri.settings.binds =
-    let
-      inherit (config.lib.niri.actions) spawn;
-      brightnessctl = lib.getExe pkgs.brightnessctl;
-    in
-    {
-      "XF86MonBrightnessUp" = {
-        action = spawn brightnessctl "set" "+5%";
-        allow-when-locked = true;
+  programs.niri.settings = {
+    outputs.eDP-1.backdrop-color = "010101";
+    binds =
+      let
+        inherit (config.lib.niri.actions) spawn;
+        brightnessctl = lib.getExe pkgs.brightnessctl;
+      in
+      {
+        "XF86MonBrightnessUp" = {
+          action = spawn brightnessctl "set" "+5%";
+          allow-when-locked = true;
+        };
+        "XF86MonBrightnessDown" = {
+          action = spawn brightnessctl "set" "5%-";
+          allow-when-locked = true;
+        };
       };
-      "XF86MonBrightnessDown" = {
-        action = spawn brightnessctl "set" "5%-";
-        allow-when-locked = true;
-      };
-    };
+  };
 }
