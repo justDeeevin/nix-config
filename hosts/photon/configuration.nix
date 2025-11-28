46ee9c4725af421a85f927cb67e859185bf4fe83
Devin Droddy <devin@justdeeevin.dev>
2025-11-28 14:12:39 -0500

    photon: add jellyfin

--- hosts/photon/configuration.nix
+++ hosts/photon/configuration.nix
@@ -2,4 +2,10 @@
   imports = [ ./hardware-configuration.nix ];
 
   users.groups.media.gid = 600;
+
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+    group = "media";
+  };
 }
