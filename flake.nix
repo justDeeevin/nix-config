e918f0c0fde1a4035ba9ebe7d7ef254b297f5610
Devin Droddy <devin@justdeeevin.dev>
2025-11-28 20:39:05 -0500

    charm: init

--- flake.nix
+++ flake.nix
@@ -140,6 +140,12 @@
           stateVersion = "25.05";
           graphical = false;
         };
+        # Homelab Caddy VM
+        charm = {
+          config = ./hosts/charm/configuration.nix;
+          stateVersion = "25.05";
+          graphical = false;
+        };
       };
       devShell.x86_64-linux = pkgs.mkShell {
         packages = with pkgs; [
