8e0a6d25d5fac439ba434f0edddd6c00a6298ee2
Devin Droddy <devin@justdeeevin.dev>
2026-02-16 21:37:30 -0500

    nixvim: add nufmt

--- nixvim/plugins/conform.nix
+++ nixvim/plugins/conform.nix
@@ -3,7 +3,10 @@
     enable = true;
     autoInstall.enable = true;
     settings = {
-      formatters_by_ft.python = [ "black" ];
+      formatters_by_ft = {
+        nu = [ "nufmt" ];
+        python = [ "black" ];
+      };
       format_on_save.lsp_format = "prefer";
     };
   };
