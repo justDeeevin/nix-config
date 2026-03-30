293a102083f136e36ce41394c7c1147f6450b724
Devin Droddy <devin@justdeeevin.dev>
2026-03-30 10:06:37 -0400

    higgs: fix missing battery display in hyprlock

--- hosts/higgs/howdy.nix
+++ hosts/higgs/howdy.nix
@@ -1,4 +1,3 @@
-{ lib, ... }:
 {
   services.howdy = {
     enable = true;
--- hosts/higgs/hypr.nix
+++ hosts/higgs/hypr.nix
@@ -1,10 +1,17 @@
+{ lib, ... }:
 {
-  programs.hyprlock.settings.label = [
+  programs.hyprlock.settings.label = lib.mkForce [
+    {
+      text = "<b>$TIME12</b>";
+      halign = "center";
+      position = "0, 5%";
+      font_size = 50;
+      font_family = "Monaspace Neon";
+    }
     {
       text = ''
         cmd[update:1000] echo "󰁹 $(cat /sys/class/power_supply/BAT0/capacity)%"
       '';
-      position = "0, 6%";
       font_size = 35;
       font_family = "Monaspace neon";
       halign = "center";
