2d0b542aed86c6faba910a1c3d929d1be8edf27d
Devin Droddy <devin@justdeeevin.dev>
2026-05-10 21:12:14 -0400

    higgs: fix nvidia prime config

--- hosts/higgs/nvidia-prime.nix
+++ hosts/higgs/nvidia-prime.nix
@@ -1,13 +1,13 @@
 {
+  services.xserver.videoDrivers = [ "modesetting" ];
   hardware.nvidia = {
     prime = {
       offload = {
         enable = true;
         enableOffloadCmd = true;
       };
-      nvidiaBusId = "PCI:1:0:0";
-      intelBusId = "PCI:0:2:0";
+      intelBusId = "PCI:0@0:2:0";
+      nvidiaBusId = "PCI:1@0:0:0";
     };
-    powerManagement.finegrained = false;
   };
 }
