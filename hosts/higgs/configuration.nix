ec02ae311ccf2c528bdea17c798b1ee93a37973a
Devin Droddy <devin@justdeeevin.dev>
2026-05-19 21:13:12 -0400

    finally get rid of the weird speaker verbs thing
    i think the kernel supports me now :3

--- hosts/higgs/configuration.nix
+++ hosts/higgs/configuration.nix
@@ -6,31 +6,6 @@
     ./howdy.nix
   ];
 
-  # Fix for internal speakers. See https://forums.fedoraforum.org/showthread.php?331130-Fixing-ALC298-audio-(no-sound-from-speakers)
-  systemd.services.speakerVerbs =
-    let
-      script = pkgs.fetchurl {
-        url = "https://github.com/joshuagrisham/galaxy-book2-pro-linux/raw/refs/heads/main/sound/necessary-verbs.sh";
-        hash = "sha256-b4liW+sNCLtssKUgRSjUxwHbvQTnOmWDQMIJZ69EMkw=";
-        executable = true;
-      };
-    in
-    {
-      wantedBy = [ "default.target" ];
-      after = [ "getty.target" ];
-      description = "Run internal speaker fix at startup";
-      serviceConfig = {
-        Type = "oneshot";
-        User = "root";
-        Group = "root";
-        RemainAfterExit = "yes";
-        ExecStart = script;
-        Restart = "on-failure";
-        RestartSec = "5s";
-      };
-      path = [ pkgs.alsa-tools ];
-    };
-
   services.upower.enable = true;
 
   services.displayManager.ly.settings =
