b60d0d7053ed08d6b3364d4b5dcc860ac3d85b26
Devin Droddy <devin@justdeeevin.dev>
2026-06-02 19:06:44 -0400

    Revert "nixcord: use quickcss for the whole theme"
    This reverts commit fbd9d77bb71cf311f64b361c073c2d61b96bee9e.
    
    fixed by nixcord#45348e2

--- global/graphical/discord.nix
+++ global/graphical/discord.nix
@@ -19,7 +19,17 @@ in
     enable = true;
 
     equibop.enable = true;
+    quickCss =
+      # css
+      ''
+        body {
+          --font: "Monaspace Neon";
+          --code-font: "Monaspace Krypton";
+        }
+      '';
     config = {
+      disableMinSize = true;
+      frameless = true;
       plugins = myLib.mkEnableList [
         {
           name = "autoZipper";
@@ -66,17 +76,9 @@ in
         "voiceMessages"
         "volumeBooster"
       ];
-
-      frameless = true;
       useQuickCss = true;
-      disableMinSize = true;
+      themes.system24-oxocarbon = builtins.readFile "${system24-oxocarbon}/system24-oxocarbon.theme.css";
+      enabledThemes = [ "system24-oxocarbon.css" ];
     };
-
-    quickCss =
-      builtins.replaceStrings
-        [ ''--font: "DM Mono"'' ''--code-font: "DM Mono"'' ]
-        [ ''--font: "Monaspace Neon"'' ''--code-font: "Monaspace Krypton"'' ]
-        (builtins.readFile "${system24-oxocarbon}/system24-oxocarbon.theme.css");
-
   };
 }
