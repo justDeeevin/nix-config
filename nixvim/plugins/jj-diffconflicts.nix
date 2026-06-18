4882d01c4e7c09a0053da168c0ae65733d6cb877
Devin Droddy <devin@justdeeevin.dev>
2026-06-18 17:55:24 -0400

    flake update
    also updates jj-diffconflicts :3

--- nixvim/plugins/jj-diffconflicts.nix
+++ nixvim/plugins/jj-diffconflicts.nix
@@ -6,8 +6,8 @@
       src = pkgs.fetchFromGitHub {
         owner = "rafikdraoui";
         repo = "jj-diffconflicts";
-        rev = "6d9eedadf1fab87bccb0a4a7ceda829da8023efb";
-        hash = "sha256-hvMXpslucywVYA9Sdxx6IcXQXYcYNWK8s9jr+KtStdI=";
+        rev = "a2aa9a247b56d2c1a6f6be81bcf41c5450cc82ff";
+        hash = "sha256-MjacjGlBRwActBBGeBZDHz8jz5J3Mt6KoDsf8WKgUDA=";
       };
     })
   ];
